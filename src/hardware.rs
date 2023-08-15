49dfe110fa20202d15f498063bf50111871ec99e
sonhs99 <sonhs99@gmail.com>
2023-08-15 09:15:08 +0900

    08/15 - done

--- src/hardware.rs
+++ src/hardware.rs
@@ -1,7 +1,9 @@
-use rustygb::{self, Cartridge, System};
+use rustygb::{self, Cartridge, Pixel, System};
 use wasm_bindgen::prelude::*;
 
 pub struct Hardware;
+
+static mut update_flag: bool = false;
 
 #[wasm_bindgen]
 pub struct Emulator {
@@ -21,40 +23,70 @@ impl Hardware {
     }
 }
 
+fn copy_buffer(frame_buffer: &mut [u8]) {
+    unsafe {
+        for idx in 0..crate::FRAME_SIZE * 4 {
+            frame_buffer[idx] = crate::frame_buffer[idx];
+        }
+    }
+}
+
 impl rustygb::Hardware for Hardware {
     fn draw_framebuffer(&mut self, frame_buffer: &rustygb::FrameBuffer) {
-        log("draw");
-        for idx in 0..crate::FRAME_SIZE {
-            unsafe {
-                crate::frame_buffer[idx] = frame_buffer.pixels[idx];
+        unsafe {
+            update_flag = true;
+            for idx in 0..crate::FRAME_SIZE {
+                (
+                    crate::frame_buffer[idx * 4],
+                    crate::frame_buffer[idx * 4 + 1],
+                    crate::frame_buffer[idx * 4 + 2],
+                    crate::frame_buffer[idx * 4 + 3],
+                ) = match frame_buffer.pixels[idx as usize] {
+                    Pixel::Black => (0x00, 0x00, 0x00, 0xFF),
+                    Pixel::Dark => (0x55, 0x55, 0x55, 0xFF),
+                    Pixel::Bright => (0xAA, 0xAA, 0xAA, 0xFF),
+                    Pixel::White => (0xFF, 0xFF, 0xFF, 0xFF),
+                };
             }
+            // log(&format!("{:X?}", crate::frame_buffer));
         }
     }
     fn get_keys(&mut self) -> (u8, u8) {
-        log("control");
-        (255, 255)
+        unsafe {
+            // log(&format!("{:X?}", (!crate::cross_button, !crate::ab_button)));
+            (crate::cross_button, crate::ab_button)
+        }
     }
     fn is_active(&mut self) -> bool {
-        log("active");
+        // log("active");
         true
     }
     fn update(&mut self) {
-        log("update");
+        // log("update");
     }
 }
 
 #[wasm_bindgen]
 impl Emulator {
-    pub fn new(rom: Vec<u8>) -> Emulator {
-        let cart = Cartridge::new(rom, vec![0; crate::RAM_SIZE]);
+    pub fn new(rom: &[u8], frame_buffer: &mut [u8]) -> Emulator {
+        let cart = Cartridge::new(rom.to_vec(), vec![0; crate::RAM_SIZE]);
         let hw = Hardware::new();
         Emulator {
             system: System::new(cart, hw),
         }
     }
 
-    pub fn step(&mut self) {
-        let value = self.system.step();
-        log(&format!("{}", value));
+    pub fn step(&mut self, frame_buffer: &mut [u8]) -> bool {
+        unsafe {
+            update_flag = false;
+            let mut clock = 0;
+            while clock < 0xFFFF && !update_flag {
+                clock += self.system.step();
+            }
+            if update_flag {
+                copy_buffer(frame_buffer);
+            }
+            update_flag
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,42 @@ const FRAME_WIDTH: usize = 144;
 const FRAME_HEIGHT: usize = 160;
 const FRAME_SIZE: usize = FRAME_WIDTH * FRAME_HEIGHT;
 
-pub static mut frame_buffer: [u32; FRAME_SIZE] = [0; FRAME_SIZE];
+pub static mut frame_buffer: [u8; FRAME_SIZE * 4] = [0; FRAME_SIZE * 4];
+pub static mut cross_button: u8 = 0;
+pub static mut ab_button: u8 = 0;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(str: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(str: &str);
+}
 
 #[wasm_bindgen]
 pub fn get_frame_buffer_ptr() -> u32 {
     unsafe { frame_buffer.as_ptr() as u32 }
 }
+
 #[wasm_bindgen]
-pub fn get_frame_buffer_size() -> u32 {
-    (FRAME_SIZE * 4) as u32
+pub fn press_button(key: u8) {
+    unsafe {
+        if key < 4 {
+            cross_button |= 1 << key;
+        } else {
+            ab_button |= 1 << (key - 4);
+        }
+        log(&format!("{:X?}", (cross_button, ab_button)));
+    }
+}
+
+#[wasm_bindgen]
+pub fn release_button(key: u8) {
+    unsafe {
+        if key < 4 {
+            cross_button &= !(1 << key);
+        } else {
+            ab_button &= !(1 << (key - 4));
+        }
+        log(&format!("{:X?}", (cross_button, ab_button)));
+    }
 }
